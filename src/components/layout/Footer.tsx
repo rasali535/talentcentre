9b6abded184c89f2ec43c24897024ef8c9141d89
rasali535 <maplininc@gmail.com>
2026-06-06 17:17:43 +0200

    Fix Vercel build error: Remove Facebook import from lucide-react and use inline SVG

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,6 @@ import {
   Mail,
   Phone,
   Globe,
-  Facebook,
   ExternalLink,
 } from 'lucide-react';
 
@@ -55,7 +54,7 @@ export default function Footer() {
                   className="w-8 h-8 rounded-lg bg-blue-600/20 border border-blue-500/30 flex items-center justify-center text-blue-400 hover:text-blue-300 transition-all duration-300"
                   title="Facebook"
                 >
-                  <Facebook className="w-4 h-4" />
+                  <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" /></svg>
                 </a>
                 <a
                   href={`https://wa.me/${(process.env.NEXT_PUBLIC_WHATSAPP_NUMBER || '+26775618647').replace(/[^0-9]/g, '')}`}
